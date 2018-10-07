918a95c3f08845c26abe9f33439f491513344498
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 12:47:32 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,6 +26,7 @@ class Scraper
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
       course.description = post.css("p").text
+      binding.pry
   end
 end
 
