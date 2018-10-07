cd3671efa7d46542e43790f68664ec03129a24df
Zachary Lehmann <zlehmann@gmail.com>
2018-10-07 12:52:15 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,6 +33,13 @@ class Scraper
     self.get_page.css(".post")
   end
 
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
 end
 
 Scraper.new.get_page
